3be4f2400381b05c835d373d37acd3843431676b
sojin13 <sodyniee@naver.com>
2019-03-08 01:13:20 +0900

    componetn commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import MyComponent from './MyComponent'; //MyComponent 불러옴
 
 
 class App extends Component {
   render() {
-    const text = "당신은 어썸한가요?"
-    const condition =true;
-    const style = {
-      backgroundColor : 'gray',
-      border: '1px solid black',
-      height: Math.round(Math.random() * 300) + 50,
-      width: Math.round(Math.random() * 300) + 50,
-      WebkitTransition: 'all',
-      MozTransition: 'all',
-      msTranstition: 'all'
-    };
-
-
     return (
+      <MyComponent/>
 
-      <div className="my-div">
-        <h1>리액트 안녕!</h1>
-        <h2>{text}</h2>
-        { condition && '보여주세요' }
-        <div style={style}></div>
-      </div>
     );
   }
 }
